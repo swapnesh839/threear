1d5ec2355e343666186d06fce9a988179b3b4c44
swapnesh <swapnesha839@gmail.com>
2024-08-08 20:50:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { XR, createXRStore } from '@react-three/xr';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import glb from './asset.glb';
+import { Canvas, useThree } from '@react-three/fiber';
+
+const store = createXRStore();
 
 function App() {
+  function Model({ color }) {
+    const { scene } = useGLTF(glb);
+    return (
+      <primitive object={scene} scale={[.9, .9, .9]} />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', position: 'relative' }}>
+      <button
+        style={{
+          position: 'absolute',
+          top: '90%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          zIndex: 999,
+          padding: '7px 12px'
+        }}
+        onClick={() => {
+          store.enterAR();
+        }}
+      >
+        Enter AR
+      </button>
+      <Canvas style={{ height: '100%' }}>
+        <ambientLight intensity={2} />
+        <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, 5, 5]} />
+        <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, -5, 5]} />
+        <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, -5, 5]} />
+        <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, 5, 5]} />
+        <OrbitControls autoRotate />
+        <XR store={store}>
+          <Model color={'#ff0000'} />
+        </XR>
+      </Canvas>
     </div>
   );
 }
