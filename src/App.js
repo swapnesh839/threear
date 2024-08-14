116ed22086d36ca1c3d82abed5cc49da0cf4af1b
swapnesh <swapnesha839@gmail.com>
2024-08-14 17:43:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import Ar from './Ar'
 import "./App.css"
 
 const App = () => {
-  const [isglview, setIsglview] = useState(false)
+  const [isglview, setIsglview] = useState(true)
 
-  useEffect(() => {
-    const element = document.documentElement;
+  // useEffect(() => {
+  //   const element = document.documentElement;
 
-    if (isglview) {
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.webkitRequestFullscreen) { /* Safari */
-        element.webkitRequestFullscreen();
-      } else if (element.msRequestFullscreen) { /* IE11 */
-        element.msRequestFullscreen();
-      }
-    } else {
-      if (document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { /* Safari */
-          document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { /* IE11 */
-          document.msExitFullscreen();
-        }
-      }
-    }
-  }, [isglview]);
+  //   if (isglview) {
+  //     if (element.requestFullscreen) {
+  //       element.requestFullscreen();
+  //     } else if (element.webkitRequestFullscreen) { /* Safari */
+  //       element.webkitRequestFullscreen();
+  //     } else if (element.msRequestFullscreen) { /* IE11 */
+  //       element.msRequestFullscreen();
+  //     }
+  //   } else {
+  //     if (document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+  //       if (document.exitFullscreen) {
+  //         document.exitFullscreen();
+  //       } else if (document.webkitExitFullscreen) { /* Safari */
+  //         document.webkitExitFullscreen();
+  //       } else if (document.msExitFullscreen) { /* IE11 */
+  //         document.msExitFullscreen();
+  //       }
+  //     }
+  //   }
+  // }, [isglview]);
 
   return (
     <React.Fragment>
@@ -38,7 +38,7 @@ const App = () => {
             style={{
               padding: '7px 12px', bottom: "70px"
             }}
-            onClick={() => setIsglview(true)}
+            // onClick={() => setIsglview(true)}
             className='btn btn-info position-absolute start-50 translate-middle-x m-2'
           >WebGl View</button>
           <img style={{ width: "160px", zIndex: 9990 }} className='position-absolute top-0 rounded-2 start-0' src='/logo.png' alt='logo' />
--- src/Ar.js
+++ src/Ar.js
@@ -1,33 +1,35 @@
 import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { XR, createXRStore } from '@react-three/xr';
-import { OrbitControls, useGLTF } from '@react-three/drei';
-import glb from './asset.glb';
+import { Html, OrbitControls, useGLTF } from '@react-three/drei';
+import glb from './pizza.glb';
 import usdz from './asset.usdz';
 import { GiExitDoor } from "react-icons/gi";
 // import "./Ar.css"
 import { Canvas } from '@react-three/fiber';
 // import { useFrame } from '@react-three/fiber';
-import CustomSlider from './useSliderColors/CustomSlider';
+// import CustomSlider from './useSliderColors/CustomSlider';
 import MoonLoader from "react-spinners/MoonLoader";
 
 const store = createXRStore();
 
-function Ar({setIsglview}) {
-  const items = [
-    { className: 'bg-danger', onClick: () => setColor('red') },
-    { className: 'bg-info', onClick: () => setColor('#0DCAF0') },
-    { style: { backgroundColor: '#6B38BB' }, onClick: () => setColor('#6B38BB') },
-    { className: 'bg-success', onClick: () => setColor('#198754') },
-    { style: { backgroundColor: 'yellow' }, onClick: () => setColor('yellow') },
-    { style: { backgroundColor: 'skyblue' }, onClick: () => setColor('skyblue') },
-    { style: { backgroundColor: 'pink' }, onClick: () => setColor('pink') },
-    { style: { backgroundColor: '#A5AC7F' }, onClick: () => setColor('#A5AC7F') },
-  ]
+function Ar({ setIsglview }) {
+  // const items = [
+  //   { className: 'bg-danger', onClick: () => setColor('red') },
+  //   { className: 'bg-info', onClick: () => setColor('#0DCAF0') },
+  //   { style: { backgroundColor: '#6B38BB' }, onClick: () => setColor('#6B38BB') },
+  //   { className: 'bg-success', onClick: () => setColor('#198754') },
+  //   { style: { backgroundColor: 'yellow' }, onClick: () => setColor('yellow') },
+  //   { style: { backgroundColor: 'skyblue' }, onClick: () => setColor('skyblue') },
+  //   { style: { backgroundColor: 'pink' }, onClick: () => setColor('pink') },
+  //   { style: { backgroundColor: '#A5AC7F' }, onClick: () => setColor('#A5AC7F') },
+  // ]
   function Model({ color }) {
     const [scale, setScale] = useState([1, 1, 1]);
     const { scene } = useGLTF(glb, true, (progress) => {
       console.log(`Loading: ${progress.loaded} / ${progress.total}`);
     });
+    console.log(scale);
+
     useGLTF.preload()
     const modelRef = useRef();
     // const [dragging, setDragging] = useState(false);
@@ -66,6 +68,7 @@ function Ar({setIsglview}) {
     // });
 
     return <primitive
+
       // onPointerDown={(e) => {
       //   setDragging(true);
       //   setInitialPointerPosition([e.clientX, e.clientY]);
@@ -83,9 +86,15 @@ function Ar({setIsglview}) {
       //     setInitialPointerPosition([e.clientX, e.clientY]);
       //   }
       // }}
-      ref={modelRef} object={scene} scale={scale} />;
+      ref={modelRef} object={scene}
+      // rotatetion={[12, 0, 0]}
+      scale={[2, 2, 2]}
+    // scale={scale}
+    />;
   }
   const [color, setColor] = useState(null)
+  console.log(setColor);
+
   const Arview = () => {
     const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     if (isIOS) {
@@ -94,21 +103,28 @@ function Ar({setIsglview}) {
       store.enterAR()
     }
   }
+  const ShowAlert = () => {
+    alert("Pizza Example Wiith Clickable function")
+  }
+
+  const [showinfo, setshowinfo] = useState(false)
+  console.log(setshowinfo);
+
   return (
     <Suspense fallback={<Loader />}>
       <div style={{ height: '100vh', position: 'relative' }} className='overflow-hidden'>
         <img style={{ width: "160px", zIndex: 9990 }} className='position-absolute top-0 rounded-2 start-0' src='/logo.png' alt='logo' />
-        <GiExitDoor onClick={() => setIsglview(false)} size={40} style={{zIndex: 9990,cursor: 'pointer', }} className='position-absolute border top-0 border-black rounded-circle p-1 end-0 m-3' src='/logo.png' alt='logo' />
+        <GiExitDoor onClick={() => setIsglview(false)} size={40} style={{ zIndex: 9990, cursor: 'pointer', }} className='position-absolute border top-0 border-black rounded-circle p-1 end-0 m-3' src='/logo.png' alt='logo' />
         <button
           style={{
-            padding: '7px 12px',bottom:"70px"
+            padding: '7px 12px', top: "10px"
           }}
           onClick={Arview}
           className='btn btn-info position-absolute start-50 translate-middle-x m-2'
         >
           View AR
         </button>
-        <CustomSlider colorItems={items} color={color} setColor={setColor} />
+        {/* <CustomSlider colorItems={items} color={color} setColor={setColor} /> */}
         {/* <div style={{ zIndex: 9999 }} className='position-absolute mt-4 d-flex bg-transparent justify-content-center align-content-center w-100 top-0 start-50 translate-middle-x'>
         <span onClick={() => setColor(null)} className='p-3 btnhvr rounded-circle  mx-2'>X</span >
         <span onClick={() => setColor('red')} className='p-3 btnhvr rounded-circle bg-danger mx-2'></span>
@@ -116,21 +132,62 @@ function Ar({setIsglview}) {
         <span onClick={() => setColor('green')} className='p-3 btnhvr rounded-circle bg-success mx-2'></span >
         </div> */}
         <Canvas id='main-canvas' style={{ height: '100%' }}>
-          <group position={[0, 0, 0]}>
+          <group position={[0, 0, 0]} rotation={[Math.PI / 6, 0, 0]} >
             <XR store={store}>
-            <ambientLight intensity={2} />
-            <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, 5, 5]} />
-            <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, -5, 5]} />
-            <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, -5, 5]} />
-            <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, 5, 5]} />
-            <OrbitControls autoRotate />
-            {/* <OrbitControls autoRotate /> */}
+              <ambientLight intensity={2} />
+              <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, 5, 5]} />
+              <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[5, -5, 5]} />
+              <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, -5, 5]} />
+              <directionalLight lookAt={[0, 0, 0]} intensity={2} position={[-5, 5, 5]} />
+              <OrbitControls />
+              {!showinfo && <Html transform position={[0, 1, -4]} zIndexRange={[0, 0]} style={{ zIndex: 100 }}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "10px",
+                    borderRadius: "15px",
+                    backgroundColor: "#FFFFFF",
+                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                    // width: "200px",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div style={{ flex: 1, marginRight: "10px" }}>
+                    <p style={{ fontSize: "14px", fontWeight: "bold", margin: 0 }}>
+                      Pizza Margherita
+                    </p>
+                    <p style={{ fontSize: "14px", fontWeight: "bold", margin: 0 }}>
+                      150 ₹
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <button
+                      onClick={() => ShowAlert()}
+                      style={{
+                        backgroundColor: "transparent",
+                        border: "1px solid black",
+                        borderRadius: "50%",
+                        width: "40px",
+                        height: "40px",
+                        zIndex: 1,
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        cursor: "pointer",
+                      }}
+                    >
+                      i
+                    </button>
+                  </div>
+                </div>
+              </Html>}
+              {/* <OrbitControls autoRotate /> */}
               <Model color={color} />
             </XR>
           </group>
         </Canvas>
       </div>
-    </Suspense>
+    </Suspense >
   );
 }
 
