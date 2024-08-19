41ae494f92ed0a7fb90b6348e852e3776b507fb5
swapnesh <swapnesha839@gmail.com>
2024-08-19 07:39:56 +0530

    dvh

--- src/Ar.js
+++ src/Ar.js
@@ -112,7 +112,7 @@ function Ar({ setIsglview }) {
 
   return (
     <Suspense fallback={<Loader />}>
-      <div style={{ height: '100vh', position: 'relative' }} className='overflow-hidden'>
+      <div style={{ height: '100dvh', position: 'relative' }} className='overflow-hidden'>
         <img style={{ width: "160px", zIndex: 9990 }} className='position-absolute top-0 rounded-2 start-0' src='/logo.png' alt='logo' />
         <GiExitDoor onClick={() => setIsglview(false)} size={40} style={{ zIndex: 9990, cursor: 'pointer' }} className='position-absolute d-none border top-0 border-black rounded-circle p-1 end-0 m-3' src='/logo.png' alt='logo' />
         <button
